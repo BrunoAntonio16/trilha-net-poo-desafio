1f1c799620696d0e9a4939c4945c61e55c81ffdc
BrunoAntonio16 <brunoantonio0210@gmail.com>
2023-12-31 14:01:32 -0300

    Modifying-Program-class

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "965473887", modelo: "NokiaK7", imei: "84324324252", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "964354632", modelo: "IphoneNG", imei: "1973285282", memoria: 120);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
